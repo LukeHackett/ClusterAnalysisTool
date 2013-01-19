1aa7b39994842970971af3b0b9c1b01a9f0fbdca
Luke Hackett <luke.hackett@live.co.uk>
2013-01-19 21:46:51 +0000

    First check in

--- src/Analysis/ProductAnalysis.cs
+++ src/Analysis/ProductAnalysis.cs
@@ -1,11 +1,37 @@
-﻿using System;
+﻿using Cluster;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
 namespace Analysis
 {
-  class ProductAnalysis
+  public class ProductAnalysis : Analysis
   {
+    #region Constructor
+    
+    /// <summary>
+    /// 
+    /// </summary>
+    public String Device { get; private set; }
+
+    #endregion
+
+
+    #region Constructor
+
+    /// <summary>
+    /// Primary Constructor
+    /// </summary>
+    /// <param name="events">A list of EventCollections representing clusters</param>
+    /// <param name="weekNumber">The week number associated with the events</param>
+    public ProductAnalysis(List<EventCollection> events, String device)
+    {
+      Events = events;
+      Device = device;
+      InitaliseAnalysis();
+    }
+
+    #endregion
   }
 }
