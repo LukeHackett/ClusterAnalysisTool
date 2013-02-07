aab3602a1887fcb72e2baeb4e59d6f1520fe397a
Luke Hackett <luke.hackett@live.co.uk>
2013-02-07 23:19:45 +0000

    Addition of new method
    Addition of GetDropFigures() which will return dropped events grouped by
    the cluster ID.
    Addition of the licence agreement.

--- src/Analysis/Analysis.cs
+++ src/Analysis/Analysis.cs
@@ -1,4 +1,31 @@
-﻿using Cluster;
+﻿/// Copyright (c) 2012, Research In Motion Limited.
+/// All rights reserved.
+/// 
+/// Redistribution and use in source and binary forms, with or without 
+/// modification, are permitted provided that the following conditions are met:
+/// 
+///  [*] Redistributions of source code must retain the above copyright notice, 
+///      this list of conditions and the following disclaimer.
+///  [*] Redistributions in binary form must reproduce the above copyright 
+///      notice, this list of conditions and the following disclaimer in the 
+///      documentation and/or other materials provided with the distribution.
+///  [*] Neither the name of the Research In Motion Limited nor the names of its 
+///      contributors may be used to endorse or promote products derived from 
+///      this software without specific prior written permission.
+/// 
+/// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
+/// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
+/// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
+/// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE 
+/// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
+/// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
+/// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
+/// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
+/// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+/// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
+/// POSSIBILITY OF SUCH DAMAGE.
+
+using Cluster;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -23,6 +50,28 @@ namespace Analysis
     #endregion
 
     #region Public Methods (Drops)
+
+    /// <summary>
+    /// This method will return all the dropped events grouped by the cluster ID.
+    /// </summary>
+    /// <returns>A Dictionary of Analysis results</returns>
+    public Dictionary<int, EventCollection> GetDropFigures()
+    {
+      // Dictionary to hold the final results in.
+      Dictionary<int, EventCollection> results = new Dictionary<int, EventCollection>();
+      // Loop over each Cluster
+      foreach (KeyValuePair<int, ClusterAnalysis> pair in ClustersAnalysis)
+      {
+        // Get the index key (Cluster ID)
+        int index = pair.Key;
+        // Get the Start RAT figures
+        EventCollection collection = pair.Value.DropAnalysis.Cluster;
+        // Add the figures to the dictionary using the cluster ID as the index
+        results.Add(index, collection);
+      }
+
+      return results;
+    }
 
     /// <summary>
     /// This method will return all the dropped events grouped by the cluster ID 
