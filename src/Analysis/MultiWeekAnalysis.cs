78cd12209cadd2c7c7547448c7e9932958987ba7
Luke Hackett <luke.hackett@live.co.uk>
2013-02-07 23:20:57 +0000

    JSON output support
    Addition of JSON output support for the analysis results
    The AddRange method has been brought in as a replacement for some of the
    overloaded Add methods.

--- src/Analysis/MultiWeekAnalysis.cs
+++ src/Analysis/MultiWeekAnalysis.cs
@@ -1,4 +1,32 @@
-﻿using Cluster;
+﻿/// Copyright (c) 2012, Research In Motion Limited.
+/// All rights reserved.
+/// 
+/// Redistribution and use in source and binary forms, with or without 
+/// modification, are permitted provided that the following conditions are met:
+/// 
+///  [*] Redistributions of source code must retain the above copyright notice, 
+///      this list of conditions and the following disclaimer.
+///  [*] Redistributions in binary form must reproduce the above copyright 
+///      notice, this list of conditions and the following disclaimer in the 
+///      documentation and/or other materials provided with the distribution.
+///  [*] Neither the name of the Research In Motion Limited nor the names of its 
+///      contributors may be used to endorse or promote products derived from 
+///      this software without specific prior written permission.
+/// 
+/// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
+/// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
+/// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
+/// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE 
+/// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
+/// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
+/// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
+/// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
+/// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+/// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
+/// POSSIBILITY OF SUCH DAMAGE.
+
+using Cluster;
+using JSON;
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -66,7 +94,7 @@ namespace Analysis
     /// removed.
     /// </summary>
     /// <param name="collection">The EventCollection to be added</param>
-    public void Add(EventCollection collection)
+    public void AddRange(EventCollection collection)
     {
       // Remove any previous clustering data
       collection.ClearAllClusterData();
@@ -80,14 +108,13 @@ namespace Analysis
     /// previous clustering information will be removed.
     /// </summary>
     /// <param name="collections">A list ov EventCollections</param>
-    public void Add(List<EventCollection> collections)
+    public void AddRange(List<EventCollection> collections)
     {
       foreach (EventCollection collection in collections)
       {
-        Add(collection);
+        AddRange(collection);
       }
     }
-
 
     /// <summary>
     /// This method will initalise the clustering algorithm, in order to cluster
@@ -102,6 +129,10 @@ namespace Analysis
 
     /// <summary>
     /// This method will analyse the clustered events upon a Multi-Week basis.
+    /// This means that events will be grouped by their week number deduced 
+    /// from the timestamp, and comparisons of events will be made from this 
+    /// aspect only. This method does not take the device name into 
+    /// consideration when grouping and analysing.
     /// </summary>
     public void AnalyseMultiWeek()
     {
@@ -135,7 +166,13 @@ namespace Analysis
       return WeeklyAnalysis[weekNumber];
     }
 
-
+    /// <summary>
+    /// This method will analyse the clustered events upon a Product basis. 
+    /// This means that products will be grouped by their device name, and
+    /// comparisons of events will be made from a product perspective. This 
+    /// method does not take the timestamps into consideration when grouping 
+    /// and analysing.
+    /// </summary>
     public void AnalyseProducts()
     {
       // Ensure the data has been clustered
@@ -151,6 +188,12 @@ namespace Analysis
       CreateProductAnalysis();
     }
 
+    /// <summary>
+    /// This method will return a ProductAnalysis object based upon the given 
+    /// device name. If the device name does not exist, an exception is thrown.
+    /// </summary>
+    /// <param name="device">The name of the device</param>
+    /// <returns>A product analysis object for the given device</returns>
     public ProductAnalysis AnalyseProduct(String device)
     {
       // Ensure that the given key exists 
@@ -160,6 +203,146 @@ namespace Analysis
       }
 
       return ProductsAnalysis[device];
+    }
+
+    /// <summary>
+    /// This method is the main entry point to create the various weekly JSON 
+    /// analysis files. A total of four files will be created, one for each 
+    /// type of analysis (DropRAT, DropMixBand, FailRAT, FailMixBand) within the 
+    /// specified output directory. Minification of the file is available to 
+    /// save space if required.
+    /// </summary>
+    /// <param name="directory">The output directory</param>
+    /// <param name="minify">Whether or not to minify the results</param>
+    public void CreateWeeklyAnalysisJSON(String directory, Boolean minify = true)
+    {
+      // Ensure there is a trailing backslash upon the directory
+      directory = directory.TrimEnd('\\') + @"\";
+
+      // Obtain the JSON String
+      String drop_rats = MergeDropRATJSON();
+      String drop_mixbands = MergeDropMixBandJSON();
+      String fail_rats = MergeFailRATJSON();
+      String fail_mixbands = MergeFailMixBandJSON();
+
+      // Check to see if the output requires prettifying (un-minifying)
+      if (!minify)
+      {
+        drop_rats = JSONWriter.PrettifyJSON(drop_rats);
+        drop_mixbands = JSONWriter.PrettifyJSON(drop_mixbands);
+        fail_rats = JSONWriter.PrettifyJSON(fail_rats);
+        fail_mixbands = JSONWriter.PrettifyJSON(fail_mixbands);
+      }
+
+      // Create each output file
+      System.IO.File.WriteAllText(directory + "week_drop_rats.json", drop_rats);
+      System.IO.File.WriteAllText(directory + "week_drop_mix_band.json", drop_mixbands);
+      System.IO.File.WriteAllText(directory + "week_fail_rats.json", fail_rats);
+      System.IO.File.WriteAllText(directory + "week_fail_mix_band.json", fail_mixbands);
+    }
+
+    /// <summary>
+    /// This method will merge all the Drop RAT JSON formatted strings for each 
+    /// given week within this object. The method will the ultimatley return a
+    /// new JSON string which will contain the multi-week analysis.
+    /// </summary>
+    /// <returns>A well formed JSON string</returns>
+    private String MergeDropRATJSON()
+    {
+      // Stores all the JSON strings for each week
+      List<String> weeks = new List<String>();
+      // Loop over each week
+      foreach (KeyValuePair<int, WeekAnalysis> pair in WeeklyAnalysis)
+      {
+        // Create the JSON String
+        String json = pair.Value.CreateDropRATJSON();
+        // Add the string to the list of weeks
+        weeks.Add(json);
+      }
+      // Create the name of the data structure
+      String name = JSONWriter.CreateKeyValue("name", "Drop Events RAT Usage");
+      // Create the children JSON array
+      String children = JSONWriter.CreateJSONArray("children", weeks);
+      // Create and return the final JSON object
+      return JSONWriter.CreateJSONObject(name, children);        
+    }
+
+    /// <summary>
+    /// This method will merge all the Drop MixBand JSON formatted strings for 
+    /// each given week within this object. The method will the ultimatley 
+    /// return a new JSON string which will contain the multi-week analysis.
+    /// </summary>
+    /// <returns>A well formed JSON string</returns>
+    private String MergeDropMixBandJSON()
+    {
+      // Stores all the JSON strings for each week
+      List<String> weeks = new List<String>();
+      // Loop over each week
+      foreach (KeyValuePair<int, WeekAnalysis> pair in WeeklyAnalysis)
+      {
+        // Create the JSON String
+        String json = pair.Value.CreateDropMixBandJSON();
+        // Add the string to the list of weeks
+        weeks.Add(json);
+      }
+      // Create the name of the data structure
+      String name = JSONWriter.CreateKeyValue("name", "Drop Events Mix-Band Usage");
+      // Create the children JSON array
+      String children = JSONWriter.CreateJSONArray("children", weeks);
+      // Create and return the final JSON object
+      return JSONWriter.CreateJSONObject(name, children);    
+    }
+
+    /// <summary>
+    /// This method will merge all the Fail RAT JSON formatted strings for each 
+    /// given week within this object. The method will the ultimatley return a
+    /// new JSON string which will contain the multi-week analysis.
+    /// </summary>
+    /// <returns>A well formed JSON string</returns>
+    private String MergeFailRATJSON()
+    {
+      // Stores all the JSON strings for each week
+      List<String> weeks = new List<String>();
+      // Loop over each week
+      foreach (KeyValuePair<int, WeekAnalysis> pair in WeeklyAnalysis)
+      {
+        // Create the JSON String
+        String json = pair.Value.CreateFailRATJSON();
+        // Add the string to the list of weeks
+        weeks.Add(json);
+      }
+      // Create the name of the data structure
+      String name = JSONWriter.CreateKeyValue("name", "Fail Events RAT Usage");
+      // Create the children JSON array
+      String children = JSONWriter.CreateJSONArray("children", weeks);
+      // Create and return the final JSON object
+      return JSONWriter.CreateJSONObject(name, children); 
+    }
+
+    /// <summary>
+    /// This method will merge all the Fail MixBand JSON formatted strings for 
+    /// each given week within this object. The method will the ultimatley 
+    /// return a new JSON string which will contain the multi-week analysis.
+    /// </summary>
+    /// <returns>A well formed JSON string</returns>
+    private String MergeFailMixBandJSON()
+    {
+      // Stores all the JSON strings for each week
+      List<String> weeks = new List<String>();
+      // Loop over each week
+      foreach (KeyValuePair<int, WeekAnalysis> pair in WeeklyAnalysis)
+      {
+        // Create the JSON String
+        String json = pair.Value.CreateFailMixBandJSON();
+        // Add the string to the list of weeks
+        weeks.Add(json);
+      }
+      // Create the name of the data structure
+      String name = JSONWriter.CreateKeyValue("name", "Fail Events Mix-Band Usage");
+      // Create the children JSON array
+      String children = JSONWriter.CreateJSONArray("children", weeks);
+      // Create and return the final JSON object
+      return JSONWriter.CreateJSONObject(name, children); 
     }
 
     #endregion
@@ -195,7 +378,10 @@ namespace Analysis
       }
     }
 
-
+    /// <summary>
+    /// This method will create the analysis object when wanting to analyse the 
+    /// events utilising the product (name) as the main pivot point.
+    /// </summary
     private void CreateProductAnalysis()
     {
       // Get a list of non-noise calls and group by device name
