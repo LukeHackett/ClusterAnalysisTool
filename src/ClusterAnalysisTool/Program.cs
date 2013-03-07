5cd0758b26de2caca66d051a72da2b7fc5d45314
Luke Hackett <luke.hackett@live.co.uk>
2013-03-07 18:08:27 +0000

    Finialisation and Commenting
    The program now supports either a single file or a directory input.
    Analysis types can be chosen - either all, week, or product.
    Additional comments have been now added.

--- src/ClusterAnalysisTool/Program.cs
+++ src/ClusterAnalysisTool/Program.cs
@@ -40,79 +40,99 @@ namespace ClusterAnalysisTool
 {
   public class Program
   {
-    private static String[] RequiredArguments = new String[] {"source", "output", "analysis"};
-    private static String[] AnalysisOptions = new String[] { "week", "product" };
+    #region Properties
 
+    /// <summary>
+    /// An array of all the minimum required arguments for the tool to work
+    /// </summary>
+    private static String[] RequiredArguments = new String[] {"source", "output", "analysis"};
+
+    /// <summary>
+    /// An array of the various analysis options
+    /// </summary>
+    private static String[] AnalysisOptions = new String[] { "week", "product", "all" };
+
+    /// <summary>
+    /// A Dictionary of the arugments that were passed to the main method
+    /// </summary>
+    private static StringDictionary InputArguments;
+
+    #endregion
+
+    #region Public Static Methods
+
+    /// <summary>
+    /// The main entry point for the console application
+    /// </summary>
+    /// <param name="args">Additional program arguments</param>
     public static void Main(string[] args)
     {
       // Get the input arguments
-      StringDictionary arguments = Arguments.GetArguments(args);
+      InputArguments = Arguments.GetArguments(args);
+
+      // Print the help contents if requested
+      if (InputArguments.ContainsKey("help"))
+      {
+        PrintHelp();
+        Environment.Exit(0);
+      }
 
       // Ensure that the minimum arguments have been passed
-      if (!HasMinimumArguements(arguments))
+      if (!HasMinimumArguements())
       {
         // Error: minimum required arguments have not beed passed
         Console.WriteLine("ERROR: Not enough arugments have been passed.");
         Console.WriteLine("Please ensure the following parameters have been " +
                           "given:");
         PrintParamArray(RequiredArguments);
+        // show the help for the user
+        PrintHelp();
         Environment.Exit(1);
       }
 
-      // Import all the files within the directory
-      EventCollection collection = GetData(arguments["source"], SearchOption.TopDirectoryOnly);
-      if (collection == null)
+      // Deduce if a file needs to be analysed or a directory
+      // Get the attributes for file or directory
+      FileAttributes fileattr = File.GetAttributes(InputArguments["source"]);
+
+      // Detect whether its a directory or file
+      if ((fileattr & FileAttributes.Directory) == FileAttributes.Directory)
       {
-        // Error: No files were found at the given directory
-        Console.WriteLine("ERROR: No input KML files were found.");
-        Console.WriteLine("Input directory: {0}", arguments["source"]);
-        Environment.Exit(1);
-      }
-
-      // Obtain the String representation of the input value
-      // Input value might be true, so ToString() method is used.
-      String analysis = arguments["analysis"].ToString();
-
-      // Perform the analysis based upon the input value
-      if(analysis == "week")
-      {
-        // Create a Multi-Week Analysis object
-        MultiWeekAnalysis multi = new MultiWeekAnalysis();
-        multi.AddRange(collection);
-        // Cluster the Multi-Week Data
-        multi.Cluster();
-        // Analyse the Multi-Week Data
-        multi.AnalyseWeeks();
-        // Save the results to the output directory
-
-        foreach (DictionaryEntry entry in arguments)
-        {
-          Console.WriteLine("{0}    {1}", entry.Key, entry.Value);
-        }
-
-        Console.WriteLine("Dictionary contains output: {0}", arguments.ContainsKey("output"));
-        Console.WriteLine(arguments["output"]);
-        String s = arguments["output"].TrimEnd('\\') + @"\";
-
-        multi.CreateWeeklyAnalysisJSON(s, false);
-
-        Console.WriteLine("Done!!!!!!!!!!");
-      }
-      else if(analysis == "product")
-      {
-
+        // Input is a directory
+        AnalyseDirectory();
       }
       else
       {
-        // Error: No valid analysis option was given
-        Console.WriteLine("ERROR: No valid analysis option was given.");
-        Console.WriteLine("A valid option is one of: ");
-        PrintParamArray(AnalysisOptions);
-        Environment.Exit(1);
+        // Input is a single file
+        AnalyseFile();
       }
+    }
 
+    #endregion
 
-      Console.WriteLine("here");
+    #region Private Static Methods
+
+    /// <summary>
+    /// This meethod will print usage instructions - more specifically how to 
+    /// use this console application.
+    /// </summary>
+    private static void PrintHelp()
+    {
+      Console.WriteLine("");
+      Console.WriteLine("Cluster Analysis Tool");
+      Console.WriteLine("Copyright (c) BlackBerry 2013. All Rights Reserved.");
+      Console.WriteLine("");
+      Console.WriteLine("USAGE:");
+      Console.WriteLine("\t --source");
+      Console.WriteLine("\t\t The source directory or .KML file");
+      Console.WriteLine("\t --output");
+      Console.WriteLine("\t\t The analysis output directory");
+      Console.WriteLine("\t --analysis=[option]");
+      Console.WriteLine("\t\t The type of analysis to be performed. " + 
+                        "option can be one of 'all', 'week' or 'product'");
+      Console.WriteLine("");
+      Console.WriteLine("\t --jsminify");
+      Console.WriteLine("\t\t Minifies all JavaScript output (default is false).");
+      Console.WriteLine("");
     }
 
     /// <summary>
@@ -121,22 +141,39 @@ namespace ClusterAnalysisTool
     /// </summary>
     /// <param name="arguments">A StringDictionary of the passed arguments</param>
     /// <returns>A boolean whether or not the arguments are correct</returns>
-    private static bool HasMinimumArguements(StringDictionary arguments)
+    private static bool HasMinimumArguements()
     {
       // Ensure that the there has been a source directory passed
-      if (!arguments.ContainsKey("source"))
+      if (!InputArguments.ContainsKey("source"))
       {
         return false;
       }
 
-      // Ensure that there has been an output directy passed
-      if (!arguments.ContainsKey("output"))
+      // Ensure the source directory/file exists
+      try
+      {
+        // Get the attributes for file or directory
+        FileAttributes fileattr = File.GetAttributes(InputArguments["source"]);
+      }
+      catch (FileNotFoundException ex)
+      {
+        Console.WriteLine("ERROR: The given input directory/file does not exist.");
+        Console.WriteLine("Source: " +  ex.FileName);
+        Environment.Exit(1);
+      }
+
+      // Ensure that there has been an output directory directy passed
+      if (!InputArguments.ContainsKey("output"))
       {
         return false;
       }
 
+      // Create the output directory if it doesn't exist
+      String output = Path.GetDirectoryName(InputArguments["output"]);
+      Directory.CreateDirectory(output);
+
       // Ensure that there has been an output directy passed
-      if (!arguments.ContainsKey("analysis"))
+      if (!InputArguments.ContainsKey("analysis"))
       {
         return false;
       }
@@ -144,36 +181,6 @@ namespace ClusterAnalysisTool
       return true;
     }
 
-    /// <summary>
-    /// 
-    /// </summary>
-    /// <param name="directory"></param>
-    /// <param name="option"></param>
-    /// <returns></returns>
-    private static EventCollection GetData(String directory, SearchOption option)
-    {
-      EventCollection collection = new EventCollection();
-
-      // Only continue if the input directory exists
-      if (!Directory.Exists(directory))
-      {
-        return null;
-      }
-
-      // Get a list of all the KML file names within the directory
-      String[] files = Directory.GetFiles(directory, "*.kml", option);
-  
-      // Loop over each of the file and add to the data
-      foreach(String file in files)
-      {
-        // Parse the KML file
-        KMLReader reader = new KMLReader(file);
-        // Add the data to the known events
-        collection.AddRange(reader.GetCallLogs());
-      }
-
-      return collection;
-    }
 
     /// <summary>
     /// This method will print each String element found within an the given 
@@ -192,5 +199,194 @@ namespace ClusterAnalysisTool
       // Print a whitespace line
       Console.WriteLine();
     }
+
+    /// <summary>
+    /// This method will analyse a given directory. It will firstly scan the 
+    /// directoy for all kml files. The analysis will produce an output KML 
+    /// file and a number of output JSON analysis files, depending upon the 
+    /// runtime options supplied.
+    /// </summary>
+    private static void AnalyseDirectory()
+    {
+      String directory = InputArguments["source"];
+
+      EventCollection collection = new EventCollection();
+
+      // Only continue if the input directory exists
+      if (!Directory.Exists(directory))
+      {
+        return;
+      }
+
+      // Get a list of all the KML file names within the directory
+      String[] files = Directory.GetFiles(directory, "*.kml", SearchOption.TopDirectoryOnly);
+
+      // Loop over each of the file and add to the data
+      foreach (String file in files)
+      {
+        // Parse the KML file
+        KMLReader reader = new KMLReader(file);
+        // Add the data to the known events
+        collection.AddRange(reader.GetCallLogs());
+      }
+
+      // Perform the analysis
+      PerformAnalysis(collection);
+
+    }
+
+    /// <summary>
+    /// This method will analyse the file that is currently stored within the 
+    /// source key of the InputArguments dictionary.
+    /// </summary>
+    private static void AnalyseFile()
+    {
+      AnalyseFile(InputArguments["source"]);
+    }
+
+    /// <summary>
+    /// This method will analyse a given kml file. The analysis will produce 
+    /// an output KML file and a number of output JSON analysis files, 
+    /// depending upon the runtime options supplied.
+    /// </summary>
+    /// <param name="file">The KML file to analyse</param>
+    private static void AnalyseFile(String file)
+    {
+      // Ensure the file exists
+      if (!File.Exists(file))
+      {
+        throw new FileNotFoundException("File does not exist", file);
+      }
+
+      // Read in all the known events from the input file
+      KMLReader reader = new KMLReader(file);
+      EventCollection collection = reader.GetCallLogs();
+
+      // Perform the analysis
+      PerformAnalysis(collection);
+    }
+
+    /// <summary>
+    /// This method will cluster and analyse a given event collection. The type 
+    /// of analysis that will be performed will depend upon the runtime 
+    /// analysis options.
+    /// </summary>
+    /// <param name="collection">The EventCollection to be clustered and analysed</param>
+    private static void PerformAnalysis(EventCollection collection)
+    {
+      // Analyse the data based upon the requirement
+      switch (InputArguments["analysis"])
+      {
+        case "week":
+          PerformWeekAnalysis(collection);
+          break;
+
+        case "product":
+          PerformProductAnalysis(collection);
+          break;
+
+        case "all":
+          PerformWeekAnalysis(collection);
+          PerformProductAnalysis(collection);
+          break;
+
+        default:
+          // Error: No valid analysis option was given
+          Console.WriteLine("ERROR: No valid analysis option was given.");
+          Console.WriteLine("A valid option is one of: ");
+          PrintParamArray(AnalysisOptions);
+          Environment.Exit(1);
+          break;
+      }
+    }
+
+    /// <summary>
+    /// This method will cluster and analyse a given event collection based 
+    /// upon a weekly approach.
+    /// </summary>
+    /// <param name="collection">The EventCollection to be clustered and analysed</param>
+    private static void PerformWeekAnalysis(EventCollection collection)
+    {
+      // Output directory
+      String output = InputArguments["output"];
+
+      // Create a Multi-Week Analysis object
+      MultiWeekAnalysis multi = new MultiWeekAnalysis();
+      multi.AddRange(collection);
+      
+      // Cluster the Multi-Week Data
+      multi.Cluster();
+      
+      // Analyse the Multi-Week Data
+      multi.AnalyseWeeks();
+
+      // Output the KML file
+      String kml_output = GenerateKMLOutputName();
+      multi.GenerateKML(kml_output);
+      
+      // Save the results to the output directory
+      bool minify = InputArguments.ContainsKey("jsminify") ? true : false;
+      multi.CreateWeeklyAnalysisJSON(output, minify);
+    }
+
+    /// <summary>
+    /// This method will cluster and analyse a given event collection based 
+    /// upon a product approach.
+    /// </summary>
+    /// <param name="collection">The EventCollection to be clustered and analysed</param>
+    private static void PerformProductAnalysis(EventCollection collection)
+    {
+      // Output directory
+      String output = InputArguments["output"];
+
+      // Create a Multi-Week Analysis object
+      MultiProductAnalysis multi = new MultiProductAnalysis();
+      multi.AddRange(collection);
+
+      // Cluster the Multi-Week Data
+      multi.Cluster();
+
+      // Analyse the Multi-Week Data
+      multi.AnalyseProducts();
+
+      // Output the KML file
+      String kml_output = GenerateKMLOutputName();
+      multi.GenerateKML(kml_output);
+
+      // Save the results to the output directory
+      bool minify = InputArguments.ContainsKey("jsminify") ? true : false;
+      multi.CreateProductsAnalysisJSON(output, minify);
+    }
+
+    /// <summary>
+    /// This method will create a KML filename based upon the runtime source 
+    /// parameter. If a file has been given, then the filename will be used as
+    /// the output KML filename. If a directory has been used, the name of the 
+    /// parent directory will be used for the output KML filename.
+    /// </summary>
+    /// <returns>A well formed KML file name</returns>
+    private static String GenerateKMLOutputName()
+    {
+      // The name of the output file
+      String name;
+
+      // Get the attributes for file or directory
+      FileAttributes fileattr = File.GetAttributes(InputArguments["source"]);
+      
+      // Detect whether its a directory or file
+      if ((fileattr & FileAttributes.Directory) == FileAttributes.Directory)
+      {
+        // Directory
+        name = Path.GetFileName(Path.GetDirectoryName(InputArguments["source"]));
+      }
+      else
+      {
+        // File 
+        name = Path.GetDirectoryName(InputArguments["source"]);
+      }
+      return InputArguments["output"] + name + "_output.kml";
+    }
+
+    #endregion
   }
 }
